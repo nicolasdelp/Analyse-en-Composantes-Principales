89e3eebec1035c69d3e36090b095e8bc75f2ae66
Delplanque Nicolas <delplanque.n@outlook.com>
2022-10-07 13:46:57 +0200

    Bon fichier

--- main.R
+++ main.R
@@ -1,24 +1,23 @@
-setwd("~/Desktop/")
 EauxFM <- read.table("EauxFM.txt", header=T, na.string="NA", dec=".", sep='\t') # Importation du fichier .txt
 
+# Verification des dimensions du jeux de donnees
 lignes <- nrow(EauxFM) # nombre de lignes (= 95)
 colonnes <- ncol(EauxFM) # nombre de colonnes (= 12)
 
-# Description univariée
-Nature <- subset(EauxFM, select=c('Nature'))
-Ca <- subset(EauxFM, select=c('Ca'))
-Mg <- subset(EauxFM, select=c('Mg'))
-Na <- subset(EauxFM, select=c('Na'))
-K <- subset(EauxFM, select=c('K'))
-Cl <- subset(EauxFM, select=c('Cl'))
-NO3 <- subset(EauxFM, select=c('NO3'))
-SO4 <- subset(EauxFM, select=c('SO4'))
-HCO3 <- subset(EauxFM, select=c('HCO3'))
-PH <- subset(EauxFM, select=c('PH'))
-Pays <- subset(EauxFM, select=c('Pays'))
+LinesNACheck <- which(is.na(EauxFM),arr.ind=TRUE)[,1] # Recuperation des lignes ayant des donnees manquantes
+LinesWithNA <- EauxFM[LinesNACheck,] # Lignes ayant des infos manquantes
+EauxFMWithoutNA <- EauxFM[-LinesNACheck,] # Donnees sans les lignes ayant des infos manquantes
 
+# Differents jeux de donnees
+FranceWithNA <- subset(EauxFM, Pays == "France") # Eaux de France
+MarocWithNA <- subset(EauxFM, Pays == "Maroc") # Eaux du Maroc
+
+# Description univariee et bivariee 
+# (Nature, Ca, Mg, Na, K, Cl, NO3, SO4, HCO3, PH, Pays)
 pie(summary(as.factor(EauxFM$Nature)))
+pie(summary(as.factor(EauxFMWithoutNA$Nature)))
+plot(EauxFM$Ca)
+plot(EauxFMWithoutNA$Ca)
 pie(summary(as.factor(EauxFM$Pays)))
 
 # Description bivariée
-
