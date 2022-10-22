81412c8454919b23b77033c967b27b0fa2729a29
Delplanque Nicolas <delplanque.n@outlook.com>
2022-10-22 13:08:32 +0200

    Nettoyage des données terminé

--- main.R
+++ main.R
@@ -1,90 +1,65 @@
 library(dplyr)
 
-##################################
-# Importation du jeux de donnees
-##################################
+#######################################
+# UTILS
+#######################################
+# Nombre de données manquantes par variable
+missingDataPerVariable <- function(datatset){
+  for (i in 1:ncol(datatset)) {
+    vec <- is.na(datatset[,i])
+    count <- sum(vec)
+    cat(names(datatset)[i] , " " , count, "\n")
+  }
+}
+
+#######################################
+# Importation du jeu de donnees
+#######################################
 setwd("C:/Users/Delpl/Desktop/ADD-Project/")
 EauxFM <- read.table("EauxFM.txt", header=T, na.string="NA", dec=".", sep='\t')
 
-# Verification de l'importation du jeux de donnees
-lignes <- nrow(EauxFM) # OK si = 95
-colonnes <- ncol(EauxFM) # OK si = 12
+# Verification de la dimension du jeu de donnees
+myDataset.dim = dim(EauxFM) # OK si 95 12
 
-##################################
-# Nettoyage
-##################################
-# Lignes ayant des infos manquantes (NA)
-LinesNACheck <- which(is.na(EauxFM),arr.ind=TRUE)[,1]
-# Recuperation dans le jeux de donnees des individus ayant des donnees manquantes
-LinesWithNA <- EauxFM[LinesNACheck,]
-# Recuperation dans le jeux de donnees des individus ayant des donnees manquantes en France
-LinesWithNAFrance <- subset(LinesWithNA, Pays == "France") 
-# Jeux de donnees sans les individus ayant des infos manquantes
-EauxFMWithoutNA <- EauxFM[-LinesNACheck,]
+#######################################
+# Nettoyage du jeu de donnees
+#######################################
+# Jeu de données principal (Eaux de France)
+France <- subset(EauxFM, Pays == "France")
+# Jeu de données supplementaire (Eaux du Maroc)
+Maroc <- subset(EauxFM, Pays == "Maroc")
 
-# Eaux de France
-FranceWithNA <- subset(EauxFM, Pays == "France") 
-# Eaux de France sans NA
-FranceWithoutNA <- subset(EauxFMWithoutNA, Pays == "France") 
-# Eaux du Maroc
-MarocWithNA <- subset(EauxFM, Pays == "Maroc") 
-# Eaux du Maroc sans NA
-MarocWithoutNA <- subset(EauxFMWithoutNA, Pays == "Maroc") 
+# Lignes ayant des donnees manquantes (NA) en France
+FranceLinesWithNA <- which(is.na(France),arr.ind=TRUE)[,1]
+FranceNA <- France[FranceLinesWithNA,]
 
-# Nombre de NA par variable
-for (i in 1:colonnes) {
-  vec <- is.na(LinesWithNAFrance[,i])
-  count <- sum(vec)
-  cat(names(LinesWithNAFrance)[i] , " " , count, "\n")
-}
+# On affiche le nombre d'individus ayant des donnees manquantes par variable
+missingDataPerVariable(FranceNA)
 
-# Il faut retirer les variables NO3 et PH car il y a 
-# +30 individus ayant un NA dans ces variables en France
+# Il faut retirer les variables NO3 et PH car il y a 30 individus ayant un NA dans ces variables en France
 
+# Jeu de données principal sans NO3 et le PH (Eaux de France)
+FranceWithoutNO3PH <- select(France,-c(NO3,PH))
 
-##################################
-# Version sans NO3 & PH
-##################################
-CleanedEauxFM <- select(EauxFM,-c(NO3,PH))
+# On affiche le nombre d'individus ayant des données manquantes par variable
+missingDataPerVariable(FranceWithoutNO3PH)
 
-# Verification du jeux de donnees
-lignes <- nrow(CleanedEauxFM) # OK si = 95
-colonnes <- ncol(CleanedEauxFM) # OK si = 12
+# On a diminue considerablement le nombre d'individus ayant des donnees manquantes
 
-# Lignes ayant des infos manquantes
-LinesNACheck <- which(is.na(CleanedEauxFM),arr.ind=TRUE)[,1]
-# Recuperation dans le jeux de donnees des individus ayant des donnees manquantes
-LinesWithNA <- CleanedEauxFM[LinesNACheck,]
-# Recuperation dans le jeux de donnees des individus ayant des donnees manquantes en France
-LinesWithNAFrance <- subset(LinesWithNA, Pays == "France") 
-# Jeux de donnees sans les individus ayant des infos manquantes
-EauxFMWithoutNA <- CleanedEauxFM[-LinesNACheck,]
+# On retire les derniers individus ayant des donnees manquantes
+FranceLinesWithNA <- which(is.na(FranceWithoutNO3PH),arr.ind=TRUE)[,1]
+myDataset.CleanFrance <- FranceWithoutNO3PH[-FranceLinesWithNA,]
 
-# Eaux de France
-FranceWithNA <- subset(CleanedEauxFM, Pays == "France") 
-# Eaux de France sans NA
-FranceWithoutNA <- subset(EauxFMWithoutNA, Pays == "France") 
-# Eaux du Maroc
-MarocWithNA <- subset(CleanedEauxFM, Pays == "Maroc") 
-# Eaux du Maroc sans NA
-MarocWithoutNA <- subset(EauxFMWithoutNA, Pays == "Maroc") 
-
-# Nombre de NA par variable
-for (i in 1:colonnes) {
-  vec <- is.na(LinesWithNAFrance[,i])
-  count <- sum(vec)
-  cat(names(LinesWithNAFrance)[i] , " " , count, "\n")
-}
+# On verifie qu'il n'y a plus d'individus ayant des donnees manquantes
+missingDataPerVariable(myDataset.CleanFrance)
 
 
+#######################################
+# Description univariée et bivariée
+#######################################
 
 
-# Description univariee et bivariee 
-# (Nature, Ca, Mg, Na, K, Cl, NO3, SO4, HCO3, PH, Pays)
-pie(summary(as.factor(EauxFM$Nature)))
-pie(summary(as.factor(EauxFMWithoutNA$Nature)))
-plot(EauxFM$Ca)
-plot(EauxFMWithoutNA$Ca)
-pie(summary(as.factor(EauxFM$Pays)))
+#######################################
+# Description multivariée
+#######################################
 
-# Description bivariée
